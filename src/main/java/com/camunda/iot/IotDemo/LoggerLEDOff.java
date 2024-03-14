2f3383370e86bb6ea1b779e60e290b7991606d35
IvanRLevyX98 <144261628+IvanRLevyX98@users.noreply.github.com>
2024-03-14 12:28:23 -0600

    Add files via upload

---
+++ src/main/java/com/camunda/iot/IotDemo/LoggerLEDOff.java
@@ -0,0 +1,20 @@
+package com.camunda.iot.IotDemo;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class LoggerLEDOff implements JavaDelegate {
+	 
+	  private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+	  
+	  public void execute(DelegateExecution execution) throws Exception {
+	    
+		LOGGER.info("\n\n  ... LoggerDelegate invoked by "
+		        + "LED Apagado" 
+		        + " \n\n");
+	    
+	  }
+
+}
